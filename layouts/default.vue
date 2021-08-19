6e15125b725bced6e40dac41844992cd733fccbd
Kolby Kruger <kolbykruger@gmail.com>
2021-08-19 19:07:15 -0400

    added Menu component

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,6 +1,7 @@
 <template>
     <div class="layout-default">
         <Header />
+        <Menu />
         <Nuxt />
         <Footer />
         <Splash />
@@ -10,6 +11,7 @@
 
 <script>
 import Header from '@/components/Header/Header.vue'
+import Menu from '@/components/Menu/Menu.vue'
 import Footer from '@/components/Footer/Footer.vue'
 import Pointer from '@/components/Pointer/Pointer.vue'
 import Splash from '@/components/Splash/Splash.vue'
@@ -17,6 +19,7 @@ import Splash from '@/components/Splash/Splash.vue'
 export default {
     components: {
         Header,
+        Menu,
         Footer,
         Pointer,
         Splash
