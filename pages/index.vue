080a5f077ef69066fa92efb42a6ad8dc6f6098ca
Kolby Kruger <kolbykruger@gmail.com>
2021-08-20 00:07:34 -0400

    added career for demo

--- pages/index.vue
+++ pages/index.vue
@@ -1,17 +1,20 @@
 <template>
     <main class="page">
         <Introduction />
+        <Career />
         <slice-zone type="page" uid="home" />
     </main>
 </template>
 
 <script>
 import Introduction from '@/components/Introduction/Introduction.vue'
+import Career from '@/components/Career/Career.vue'
 import SliceZone from 'vue-slicezone'
 
 export default {
     components: {
         Introduction,
+        Career,
         SliceZone
     }
 }
