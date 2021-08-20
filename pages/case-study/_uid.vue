c1021d94ee05db231804f518c56a16c96998941d
Kolby Kruger <kolbykruger@gmail.com>
2021-08-20 18:39:59 -0400

    slight adjustments to case-study

--- pages/case-study/_uid.vue
+++ pages/case-study/_uid.vue
@@ -1,6 +1,6 @@
 <template>
     <main class="page case-study">
-        <Pageheading :name="document.data.Name" />
+        <Pageheading :name="document.data.Name" :capital="true" />
 
         <section class="cs-intro">
             <div class="container">
@@ -12,7 +12,6 @@
                         <p class="cs-website" v-if="document.data.Website">
                             <a
                                 data-magnetic
-                                data-exclusion
                                 :href="document.data.Website.url"
                                 target="_blank"
                                 rel="nofollow norefer"
@@ -30,7 +29,7 @@
                                         fill-rule="evenodd"
                                         clip-rule="evenodd"
                                         d="M7.64997 6.00002C7.64997 5.5858 7.98576 5.25002 8.39997 5.25002H18C18.4142 5.25002 18.75 5.5858 18.75 6.00002V15.6C18.75 16.0142 18.4142 16.35 18 16.35C17.5858 16.35 17.25 16.0142 17.25 15.6V7.81068L6.53033 18.5303C6.23744 18.8232 5.76257 18.8232 5.46967 18.5303C5.17678 18.2375 5.17678 17.7626 5.46967 17.4697L16.1893 6.75002H8.39997C7.98576 6.75002 7.64997 6.41423 7.64997 6.00002Z"
-                                        fill="#22282F"
+                                        :style="{ fill: document.data.Accent ? document.data.Accent : '#000' }"
                                     />
                                 </svg>
                             </a>
@@ -69,6 +68,18 @@
         <slice-zone type="case-study" :uid="$route.params.uid" />
 
         <Neighbor :document="document" />
+
+        <div class="cs-return">
+            <nuxt-link to="/work">
+                <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none">
+                    <path
+                        d="M12.4569 4.02802C12.281 3.63211 11.7191 3.63211 11.5431 4.02802L4.48451 19.9099C4.30442 20.3151 4.70178 20.7368 5.11698 20.5811L11.8245 18.0658C11.9377 18.0234 12.0624 18.0234 12.1756 18.0658L18.8831 20.5811C19.2983 20.7368 19.6956 20.3151 19.5155 19.9099L12.4569 4.02802Z"
+                        fill="#22282F"
+                    />
+                </svg>
+                <span>Projects</span>
+            </nuxt-link>
+        </div>
     </main>
 </template>
 
@@ -140,13 +151,21 @@ export default {
 
         a {
             color: currentColor;
+            display: inline-flex;
+            align-items: center;
+            height: 64px;
+
+            svg {
+                margin-left: 0.4em;
+                margin-bottom: 0.2em;
+                width: 32px;
+                height: 32px;
+            }
         }
     }
 
     &-stats {
         &-container {
-            position: sticky;
-            top: 10vh;
         }
 
         &-group {
@@ -164,6 +183,32 @@ export default {
         &-label,
         &-item {
             font-size: clamp(1.25rem, -0.875rem + 8.333vw, 1.414rem);
+        }
+    }
+
+    &-return {
+        position: fixed;
+        bottom: 2em;
+        left: 4em;
+        transform: rotate(-90deg);
+        transform-origin: bottom left;
+        opacity: 0.125;
+
+        a {
+            display: inline-flex;
+            align-items: center;
+            justify-content: center;
+            text-decoration: none;
+            color: #000;
+            font-size: 1.5rem;
+            letter-spacing: 0.05em;
+            text-transform: uppercase;
+            font-weight: 500;
+
+            svg {
+                margin-right: 0.4em;
+                margin-bottom: 0.2em;
+            }
         }
     }
 }
