b2efe4cedc074704d4557f60bf73b42f20df96fe
Kolby Kruger <kolbykruger@gmail.com>
2021-08-21 23:07:43 -0400

    added `style-resources` plugin

--- nuxt.config.js
+++ nuxt.config.js
@@ -44,6 +44,9 @@ export default {
         '@/assets/fonts/ogg/ogg.css',
         '@/assets/fonts/tt-commons/tt-commons.css'
     ],
+    styleResources: {
+        scss: ['@/assets/css/internals.scss']
+    },
     // Plugins to run before rendering page (https://go.nuxtjs.dev/config-plugins)
     plugins: [{ src: '~/plugins/flickity.js', ssr: false }],
     // Auto import components (https://go.nuxtjs.dev/config-components)
@@ -55,6 +58,7 @@ export default {
     // Modules (https://go.nuxtjs.dev/config-modules)
     modules: [
         '@nuxt/image',
+        '@nuxtjs/style-resources',
         [
             '@nuxtjs/prismic',
             {
