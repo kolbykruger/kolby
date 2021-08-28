4ce2ca1cc714db4bea13d0ccaa7c3e9c5d6a58cf
Kolby Kruger <kolbykruger@gmail.com>
2021-08-28 17:44:13 -0400

    added an error layout

---
+++ layouts/error.vue
@@ -0,0 +1,26 @@
+<template>
+    <main class="page">
+        <section>
+            <div class="container">
+                <Pageheading v-if="error.statusCode === 404" :name="err" />
+                <NuxtLink to="/">Home page</NuxtLink>
+            </div>
+        </section>
+    </main>
+</template>
+
+<script>
+import Pageheading from '@/components/Pageheading/Pageheading'
+export default {
+    props: ['error'],
+    //layout: 'error',
+    components: {
+        Pageheading
+    },
+    computed: {
+        err() {
+            return this.error.statusCode === 404 ? 'Page not found' : 'An error occurred'
+        }
+    }
+}
+</script>
