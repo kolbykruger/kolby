2457dd7467afa4321d5636e663d5d2319faae56a
Kolby Kruger <kolbykruger@gmail.com>
2021-08-28 17:44:51 -0400

    more testing with shapes

--- mixins/intro/Canvas.js
+++ mixins/intro/Canvas.js
@@ -2,84 +2,142 @@ import * as THREE from 'three'
 //import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js'
 var OrbitControls = require('three-orbit-controls')(THREE)
 import { AmbientLight, Camera, WebGLRenderer } from 'three'
+import threeOrbitControls from 'three-orbit-controls'
 
 export const Canvas = {
     name: 'Canvas',
     data() {
-        return {}
+        return {
+            mouse: {
+                x: 0,
+                y: 0
+            },
+            scene: {
+                width: 0,
+                height: 0,
+                halfWidth: 0,
+                halfHeight: 0
+            }
+        }
     },
     methods: {},
     mounted() {
+        // Screen
+        this.screen = {
+            width: window.innerWidth,
+            height: window.innerHeight,
+            halfWidth: window.innerWidth / 2,
+            halfHeight: window.innerHeight / 2
+        }
+
+        // Mouse
+        window.addEventListener('mousemove', event => {
+            this.mouse = {
+                x: event.clientX - this.scene.halfWidth,
+                y: event.clientY - this.scene.halfHeight
+            }
+        })
+
         // Definitions
         const canvas = this.$refs.canvas
         const scene = new THREE.Scene()
 
         // Materials
-        const sphereMaterial = new THREE.MeshStandardMaterial({
-            color: 0xff401e,
-            metalness: 0.2,
-            roughness: 0.4
+        const material = new THREE.MeshStandardMaterial({
+            color: 0xdddddd,
+            metalness: 0,
+            roughness: 1,
+            wireframe: false
         })
-        const torusMaterial = new THREE.MeshStandardMaterial({
-            color: 0xffdb69,
-            metalness: 0.2,
-            roughness: 0.4
-        })
-        // material.metalness = 0.7
-        // material.roughness = 0.2
+
+        material.side = THREE.DoubleSide
+        material.color = new THREE.Color(0xf7768e)
+        const mat1 = material
+
+        // material.color = new THREE.Color(0xffdb69)
+        // const mat2 = material
+
+        // material.color = new THREE.Color(0xb267e6)
+        // const mat3 = material
+
+        // material.color = new THREE.Color(0x11c9c3)
+        // const mat4 = material
+
+        // material.color = new THREE.Color(0x89ddff)
+        // const mat5 = material
+
+        // material.color = new THREE.Color(0x444b6a)
+        // const mat6 = material
 
         // Shapes
-        const sphere = new THREE.Mesh(new THREE.SphereGeometry(0.5, 128, 128), sphereMaterial)
-        sphere.position.x = -1.5
+        const geo = new THREE.Mesh(new THREE.IcosahedronGeometry(500, 1), mat1)
+        const geo1 = geo.clone()
+        geo1.position.x = 1000
+        const geo2 = geo.clone()
+        geo2.position.x = -1000
 
-        //const plane = new THREE.Mesh(new THREE.mesh.PlaneGeometry(1, 1, 100, 100), material)
-        //plane.geometry.setAttribute('uv2', new THREE.BufferAttribute(plane.geometry.attributes.uv.array, 2))
+        // const hemisphere = new THREE.Mesh(
+        //     new THREE.SphereBufferGeometry(200, 8, 6, 0, 2 * Math.PI, 0, 0.5 * Math.PI),
+        //     mat1
+        // )
 
-        const torus = new THREE.Mesh(new THREE.TorusGeometry(0.5, 0.2, 128, 256), torusMaterial)
-        torus.position.x = 1.5
+        // const knot = new THREE.Mesh(new THREE.TorusKnotGeometry(0.25, 0.25, 128, 12, 2, 3), mat1)
 
-        scene.add(sphere, torus)
+        // const cylinder = new THREE.Mesh(new THREE.CylinderGeometry(0.5, 0.5, 1, 5), cylinderMaterial)
+        // cylinder.position.x = 0
+        // cylinder.position.y = -0.5
+
+        // const box = new THREE.Mesh(new THREE.BoxGeometry(0.75, 0.75, 0.75), boxMaterial)
+        // box.position.x = 1
+        // box.position.y = -0.5
+
+        // const cone = new THREE.Mesh(new THREE.ConeGeometry(0.5, 1, 32), coneMaterial)
+        // cone.position.x = -1
+        // cone.position.y = 0.5
+
+        // const semiSphere = new THREE.Mesh(
+        //     new THREE.SphereBufferGeometry(0.5, 128, 128, Math.PI / 2, Math.PI * 2, 0, Math.PI),
+        //     semisphereMaterial
+        // )
+        // semiSphere.position.x = 1
+        // semiSphere.position.y = 0.5
+
+        // scene.add(sphere, semiSphere, cylinder, cone, box, knot)
+        scene.add(geo, geo1, geo2)
 
         // Lighting
-        const ambientLight = new THREE.AmbientLight(0xffffff, 0.2)
+        const ambientLight = new THREE.AmbientLight(0xffffff, 0.15)
         scene.add(ambientLight)
 
-        const pointLight = new THREE.PointLight(0xffffff, 0.4)
-        pointLight.position.x = 2
-        pointLight.position.y = 5
-        pointLight.position.z = 4
+        const pointLight = new THREE.PointLight(0xffffff, 0.7)
+        pointLight.position.x = 3000
+        pointLight.position.y = 6000
+        pointLight.position.z = 5000
         scene.add(pointLight)
-
-        //Sizing
-        const sizes = {
-            width: window.innerWidth,
-            height: window.innerHeight
-        }
 
         window.addEventListener('resize', () => {
             // Update sizes
-            sizes.width = window.innerWidth
-            sizes.height = window.innerHeight
+            this.screen.width = window.innerWidth
+            this.screen.height = window.innerHeight
 
             // Update camera
-            camera.aspect = sizes.width / sizes.height
+            camera.aspect = this.screen.width / this.screen.height
             camera.updateProjectionMatrix()
 
             // update renderer
-            renderer.setSize(sizes.width, sizes.height)
+            renderer.setSize(this.screen.width, this.screen.height)
             renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
         })
 
         // Camera
-        const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height, 0.1, 100)
-        camera.position.x = 1
-        camera.position.y = 1
-        camera.position.z = 3
+        const camera = new THREE.PerspectiveCamera(20, this.screen.width / this.screen.height, 1, 10000)
+        camera.position.z = 1800
         scene.add(camera)
+        console.log(camera.position)
 
         // Controls
-        // const controls = new OrbitControls(camera, canvas)
-        // controls.enableDamping = true
+        const controls = new OrbitControls(camera, canvas)
+        controls.enableDamping = true
 
         // Renderer
         const renderer = new THREE.WebGLRenderer({
@@ -87,7 +145,7 @@ export const Canvas = {
             alpha: true
         })
 
-        renderer.setSize(sizes.width, sizes.height)
+        renderer.setSize(this.screen.width, this.screen.height)
         renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
         renderer.setClearColor(0x000000, 0)
 
@@ -98,13 +156,28 @@ export const Canvas = {
             const elapsedTime = clock.getElapsedTime()
 
             // Update Objects
-            sphere.rotation.y = 0.1 * elapsedTime
-            torus.rotation.y = 0.1 * elapsedTime
-            sphere.rotation.x = 0.15 * elapsedTime
-            torus.rotation.x = 0.15 * elapsedTime
+            // sphere.rotation.y = 0.1 * elapsedTime
+            // cylinder.rotation.y = 0.1 * elapsedTime
+            // semiSphere.rotation.y = 0.1 * elapsedTime
+            // cone.rotation.y = 0.1 * elapsedTime
+            // box.rotation.y = 0.1 * elapsedTime
+            // knot.rotation.y = 0.1 * elapsedTime
+
+            // sphere.rotation.x = 0.15 * elapsedTime
+            // cylinder.rotation.x = 0.15 * elapsedTime
+            // semiSphere.rotation.x = 0.15 * elapsedTime
+            // cone.rotation.x = 0.15 * elapsedTime
+            // box.rotation.x = 0.15 * elapsedTime
+            // knot.rotation.x = 0.15 * elapsedTime
+
+            // camera.position.x += (this.mouse.x - camera.position.x) * 0.05
+            // camera.position.y += (-this.mouse.y - camera.position.y) * 0.05
+            // camera.lookAt(scene.position)
+
+            // console.log(camera.position)
 
             // Update controls
-            //controls.update()
+            controls.update()
 
             // Update Renderer
             renderer.render(scene, camera)
