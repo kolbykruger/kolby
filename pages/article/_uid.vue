0b78805b03fb5cbe4de2faf1f85349610e9035cf
Kolby Kruger <kolbykruger@gmail.com>
2021-08-28 17:45:56 -0400

    created article page

---
+++ pages/article/_uid.vue
@@ -0,0 +1,169 @@
+<template>
+    <main class="page case-study">
+        <Pageheading :name="document.data.Name" />
+
+        <article class="article">
+            <div class="container">
+                <div class="article-grid">
+                    <!-- <aside class="article-track"></aside> -->
+                    <div class="article-content">
+                        <slice-zone type="article" :uid="$route.params.uid" />
+                    </div>
+                    <aside class="article-track">
+                        <div data-sticky class="article-details">
+                            <div class="article-details-container">
+                                <ul class="article-details-group">
+                                    <li class="article-details-label">Date</li>
+                                    <li class="article-details-item">
+                                        <span>{{ formatDate }}</span>
+                                    </li>
+                                </ul>
+
+                                <ul class="article-details-group">
+                                    <li class="article-details-label">Read time</li>
+                                    <li class="article-details-item">
+                                        <span>{{ document.data.Read + ' minutes' || 'Read time' }}</span>
+                                    </li>
+                                </ul>
+
+                                <ul class="article-details-group">
+                                    <li class="article-details-label">Category</li>
+                                    <li class="article-details-item">
+                                        <span>{{ document.data.Category || 'Category' }}</span>
+                                    </li>
+                                </ul>
+                            </div>
+                        </div>
+                    </aside>
+                </div>
+            </div>
+        </article>
+
+        <pre><code>{{ document }}</code></pre>
+    </main>
+</template>
+
+<script>
+import SliceZone from 'vue-slicezone'
+import Pageheading from '~/components/Pageheading/Pageheading.vue'
+
+export default {
+    components: {
+        SliceZone,
+        Pageheading
+    },
+    computed: {
+        formatDate() {
+            const date = new Date(this.document.last_publication_date)
+            const options = {
+                year: 'numeric',
+                day: 'numeric',
+                month: 'long'
+            }
+            return date.toLocaleDateString('en-us', options)
+        }
+    },
+    async asyncData({ $prismic, params, error }) {
+        const document = await $prismic.api.getByUID('article', params.uid)
+        // const nextArticle = (
+        //     await $prismic.api.query($prismic.predicates.at('document.type', 'case-study'), {
+        //         pageSize: 3,
+        //         after: `${document.id}`,
+        //         orderings: '[my.post.date]'
+        //     })
+        // ).results[0]
+        if (document) {
+            return { document }
+        } else {
+            error({ statusCode: 404, message: 'Page not found' })
+        }
+    }
+}
+</script>
+
+<style lang="scss">
+@import url('https://fonts.googleapis.com/css2?family=Fira+Mono:wght@500&display=swap');
+
+.article {
+    .article-grid {
+        display: grid;
+        grid-template-columns: auto 800px auto;
+        grid-template-columns: 800px auto;
+        grid-gap: 6vw;
+    }
+
+    .article-track {
+        > [data-sticky] {
+            position: sticky;
+            top: 10vh;
+        }
+    }
+
+    .article-content {
+        > div:first-of-type {
+            h2,
+            h3,
+            p {
+                margin-top: 0;
+            }
+        }
+    }
+
+    section {
+        position: relative;
+        margin: 0 auto;
+
+        .container {
+            display: flex;
+            flex-direction: column;
+            align-items: center;
+            padding: 0;
+
+            > * {
+                min-width: 100%;
+                width: 100%;
+            }
+        }
+    }
+
+    h2 {
+        font-family: $font-1;
+        font-size: clamp(2.827rem, -0.875rem + 8.333vw, 3.998rem);
+        font-weight: 600;
+    }
+
+    h3 {
+        font-size: clamp(1.999rem, -0.875rem + 8.333vw, 2.827rem);
+    }
+
+    h4 {
+        font-size: clamp(1.414rem, -0.875rem + 8.333vw, 1.999rem);
+    }
+
+    h5 {
+        font-size: clamp(1.414rem, -0.875rem + 8.333vw, 1.414rem);
+    }
+
+    &-details {
+        &-container {
+        }
+
+        &-group {
+            margin-bottom: 3vh;
+        }
+
+        &-label {
+            font-weight: 500;
+        }
+
+        &-item {
+            opacity: 0.75;
+        }
+
+        &-label,
+        &-item {
+            font-size: clamp(1.25rem, -0.875rem + 8.333vw, 1.414rem);
+        }
+    }
+}
+</style>
