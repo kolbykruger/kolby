e82454a465045a6f97c2b7ced4feb5ca0c349593
Kolby Kruger <kolbykruger@gmail.com>
2021-08-28 17:47:07 -0400

    created

---
+++ slices/ArticleBlock/index.vue
@@ -0,0 +1,27 @@
+<template>
+    <section class="article-block">
+        <div class="container">
+            <prismic-rich-text :field="slice.primary.Content" />
+        </div>
+    </section>
+</template>
+
+<script>
+export default {
+    name: 'ArticleBlock',
+    props: {
+        slice: {
+            type: Object,
+            required: true,
+            default() {
+                return {}
+            }
+        }
+    }
+}
+</script>
+
+<style lang="scss">
+.article-block {
+}
+</style>
