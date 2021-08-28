7c759ede00f418777dde55a4b83ea2f474ffc331
Kolby Kruger <kolbykruger@gmail.com>
2021-08-28 17:45:00 -0400

    testing metadata for case-studies

---
+++ mixins/meta/CaseStudyMeta.js
@@ -0,0 +1,24 @@
+export const CaseStudyMeta = {
+    name: 'CaseStudyMeta',
+    computed: {
+        title() {
+            //return document.data.Name
+            return this.document.data.Name[0].text + ' - Kolby Kruger'
+        },
+        description() {
+            return this.$prismic.asText(this.document.data.Summary)
+        }
+    },
+    head() {
+        return {
+            title: this.title,
+            meta: [
+                {
+                    hid: 'description',
+                    name: 'description',
+                    content: this.description
+                }
+            ]
+        }
+    }
+}
