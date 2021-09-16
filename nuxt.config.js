ca91563380288c20dc1468a8b898ab3188bca594
Kolby Kruger <kolbykruger@gmail.com>
2021-09-16 01:38:56 -0400

    corrected module statement

--- nuxt.config.js
+++ nuxt.config.js
@@ -56,13 +56,12 @@ export default {
     // Auto import components (https://go.nuxtjs.dev/config-components)
     components: true,
     // Modules for dev and build (recommended) (https://go.nuxtjs.dev/config-modules)
-    buildModules: ['@nuxt/image'],
+    buildModules: ['@nuxt/image', '@nuxtjs/style-resources'],
     // Transpile
-    transpile: ['gsap', 'three', ''],
+    transpile: ['gsap', 'three'],
     // Modules (https://go.nuxtjs.dev/config-modules)
     modules: [
         '@nuxt/image',
-        '@nuxtjs/style-resources',
         [
             '@nuxtjs/prismic',
             {
