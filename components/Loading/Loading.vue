8cc0f89a2db63c827b5579884c902e7b899f2f55
Kolby Kruger <kolbykruger@gmail.com>
2021-09-16 23:46:08 -0400

    created loading component

---
+++ components/Loading/Loading.vue
@@ -0,0 +1,33 @@
+<template>
+    <div class="loadingstate">
+        {{ loading }}
+    </div>
+</template>
+
+<script>
+export default {
+    data: () => ({
+        loading: false
+    }),
+    methods: {
+        start() {
+            this.loading = true
+            this.$store.commit('loading/setLoading', { status: true })
+        },
+        finish() {
+            this.loading = false
+            this.$nextTick(() => {
+                this.$store.commit('loading/setLoading', { status: false })
+            })
+        }
+    }
+}
+</script>
+<style>
+.loadingstate {
+    position: fixed;
+    z-index: 999999999999;
+    top: 0;
+    left: 0;
+}
+</style>
