6534e086e31c6bb37779b0a2e90834c342bf5aeb
Kolby Kruger <kolbykruger@gmail.com>
2021-09-20 12:55:15 -0400

    initial stylizing

--- components/Loading/Loading.vue
+++ components/Loading/Loading.vue
@@ -1,6 +1,6 @@
 <template>
-    <div class="loadingstate">
-        {{ loading }}
+    <div v-if="loading" class="loading-page">
+        <div class="loading"></div>
     </div>
 </template>
 
@@ -24,10 +24,32 @@ export default {
 }
 </script>
 <style>
-.loadingstate {
+.loading-page {
     position: fixed;
-    z-index: 999999999999;
     top: 0;
-    left: 0;
+    right: 0;
+    z-index: 1000;
+    padding: 1rem;
+    text-align: center;
+    font-size: 3rem;
+    font-family: sans-serif;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+}
+.loading {
+    display: inline-block;
+    width: 1.5rem;
+    height: 1.5rem;
+    border: 4px solid rgba(9, 133, 81, 0.705);
+    border-radius: 50%;
+    border-top-color: #158876;
+    animation: spin 1s ease-in-out infinite;
+}
+@keyframes spin {
+    to {
+        -webkit-transform: rotate(360deg);
+    }
 }
 </style>
