f10be14f9b79808d189c9aedcc448150fae62885
Kolby Kruger <kolbykruger@gmail.com>
2021-09-20 12:56:05 -0400

    added more details to articles

--- pages/articles.vue
+++ pages/articles.vue
@@ -4,34 +4,62 @@
 
         <section class="articles">
             <div class="container">
-                <ul class="articles-list">
-                    <li class="article-item" v-for="article in articles.results" :key="article.id">
-                        <nuxt-link data-cursor="lg" :to="'/article/' + article.uid">
-                            <!-- <p class="article-item-category">Self</p> -->
-                            <h2 class="article-item-title">{{ article.data.Name[0].text }}</h2>
+                <div class="articles-grid">
+                    <ul class="articles-list" v-if="articles">
+                        <li class="article-item" v-for="article in filteredArticles" :key="article.id">
+                            <nuxt-link data-cursor="lg" :to="'/article/' + article.uid">
+                                <!-- <p class="article-item-category">Self</p> -->
+                                <h2 class="article-item-title">{{ article.data.Name[0].text }}</h2>
 
-                            <prismic-rich-text class="article-item-summary" :field="article.data.Summary" />
+                                <prismic-rich-text class="article-item-summary" :field="article.data.Summary" />
 
-                            <div class="article-item-details">
-                                <p class="article-item-date">
-                                    {{ formatDate(article.last_publication_date) }}
-                                </p>
-                                <span v-if="article.data.Read"> — </span>
-                                <p class="article-item-read" v-if="article.data.Read">
-                                    {{ article.data.Read }} min read
-                                </p>
-                                <span v-if="article.data.Category"> — </span>
-                                <p class="article-item-category" v-if="article.data.Category">
-                                    {{ article.data.Category }}
-                                </p>
-                            </div>
-                        </nuxt-link>
-                    </li>
-                </ul>
+                                <div class="article-item-details">
+                                    <p class="article-item-date">
+                                        {{ formatDate(article.last_publication_date) }}
+                                    </p>
+                                    <span v-if="article.data.Read"> — </span>
+                                    <p class="article-item-read" v-if="article.data.Read">
+                                        {{ article.data.Read }} min read
+                                    </p>
+                                    <span v-if="article.data.Category"> — </span>
+                                    <p class="article-item-category" v-if="article.data.Category">
+                                        {{ article.data.Category }}
+                                    </p>
+                                </div>
+                            </nuxt-link>
+                        </li>
+                    </ul>
+                    <ul class="articles-categories">
+                        <li>
+                            <p>Categories</p>
+                        </li>
+                        <li>
+                            <button
+                                data-stick
+                                data-cursor="lg"
+                                class="button"
+                                :class="{ '-active': category == 'all' }"
+                                @click="$router.push({ path: '/articles' })"
+                            >
+                                Latest
+                            </button>
+                        </li>
+                        <li v-for="(item, index) in categories" :key="index">
+                            <button
+                                data-stick
+                                data-cursor="lg"
+                                class="button"
+                                :class="{ '-active': category.toLowerCase() == item.toLowerCase() }"
+                                @click="$router.push({ path: '/articles', query: { category: item } })"
+                            >
+                                {{ item }}
+                            </button>
+                        </li>
+                        <li v-if="articles">{{ filteredArticles.length }} articles</li>
+                    </ul>
+                </div>
             </div>
         </section>
-
-        <pre><code>{{ articles.results }}</code></pre>
     </main>
 </template>
 
@@ -40,6 +68,11 @@
 import Pageheading from '~/components/Pageheading/Pageheading.vue'
 
 export default {
+    data() {
+        return {
+            categories: ['Code', 'Guide']
+        }
+    },
     components: {
         Pageheading
     },
@@ -62,6 +95,22 @@ export default {
             }
             return date.toLocaleString('en-us', options)
         }
+    },
+    computed: {
+        filteredArticles() {
+            const category = this.$route.query.category
+            const articles = this.articles
+
+            return category
+                ? articles.results.filter(a => {
+                      return a.data.Category.toLowerCase() == category.toLowerCase()
+                  })
+                : articles.results
+        },
+        category() {
+            const category = this.$route.query.category
+            return category ? category : 'all'
+        }
     }
 }
 </script>
@@ -71,10 +120,22 @@ export default {
     .container {
     }
 
+    &-grid {
+        display: grid;
+        grid-template-columns: 800px 250px;
+        grid-gap: 6em;
+    }
+
     &-list {
         list-style: none;
         padding: 0;
         margin: 0;
+    }
+
+    &-categories {
+        // display: flex;
+        // align-items: center;
+        // margin-bottom: 5vh;
     }
 }
 
