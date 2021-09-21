46b08a88eb5b53c67db7896d5f57f9278078e689
Kolby Kruger <kolbykruger@gmail.com>
2021-09-21 14:05:38 -0400

    Created Author component

---
+++ components/Author/Author.vue
@@ -0,0 +1,49 @@
+<template>
+    <div class="author">
+        <div class="author-image">
+            <img src="/images/kolby.png" alt="Kolby Kruger - smiling." />
+        </div>
+        <div class="author-name">
+            <p><small>Kolby Kruger</small></p>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'Author'
+}
+</script>
+
+<style lang="scss">
+.author {
+    margin-bottom: 3vh;
+
+    &-image {
+        --size: 60px;
+        width: var(--size);
+        height: var(--size);
+        overflow: hidden;
+
+        img {
+            width: 100%;
+            height: 100%;
+            object-fit: cover;
+            border-radius: 50%;
+            background: c('menu-color');
+        }
+    }
+
+    &-name {
+        margin-top: 1em;
+        margin-bottom: 0;
+        color: c('base-5');
+        font-weight: 500;
+
+        small {
+            color: c('base-5');
+            font-weight: 500;
+        }
+    }
+}
+</style>
