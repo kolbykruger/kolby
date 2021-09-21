ab0470028c4b7a344122e4c542ae20e5db5cbbc3
Kolby Kruger <kolbykruger@gmail.com>
2021-09-21 14:05:44 -0400

    added decor to footer

--- components/Footer/Footer.vue
+++ components/Footer/Footer.vue
@@ -1,6 +1,10 @@
 <template>
     <footer class="footer" ref="footer" data-traveler="transparent">
         <div class="container">
+            <div class="footer-magic">
+                <span class="footer-magic-artwork"></span>
+            </div>
+
             <ul class="footer-links">
                 <li>
                     <a data-stick data-cursor="xl" href="#">
@@ -42,19 +46,18 @@
                 </li>
                 <li>
                     <a data-stick data-cursor="xl" href="#">
-                        <span>CV</span>
+                        <span>Spotify</span>
                         <svg
                             xmlns="http://www.w3.org/2000/svg"
-                            width="97"
-                            height="141"
-                            viewBox="0 0 97 141"
+                            width="168"
+                            height="169"
+                            viewBox="0 0 168 169"
                             fill="none"
-                            class="-tiny"
                         >
                             <path
                                 fill-rule="evenodd"
                                 clip-rule="evenodd"
-                                d="M55.1999 100.055C64.8646 85.7176 79.0381 74.9801 94.9975 74.1447C96.1006 74.0869 97 74.9877 97 76.0923L97 85.0081C97 86.1126 96.1024 87.0001 95.0014 87.0888C74.253 88.7602 56.2656 116.08 55.2456 138.661C55.1958 139.764 54.3046 140.658 53.2 140.658L53 140.658L44 140.658L43.8 140.658C42.6954 140.658 41.8042 139.764 41.7546 138.66C40.7395 116.077 22.8338 88.7529 1.99862 87.0881C0.897561 87.0001 -6.94524e-06 86.1126 -6.89696e-06 85.0081L-6.50723e-06 76.0923C-6.45895e-06 74.9877 0.899431 74.0869 2.00249 74.1447C17.9619 74.9801 32.1353 85.7175 41.7999 100.054L41.8 2.6582C41.8 1.55363 42.6954 0.658201 43.8 0.658201L53.1999 0.658201C54.3045 0.658201 55.1999 1.55363 55.1999 2.6582L55.1999 100.055Z"
+                                d="M99.841 58.9134C82.8693 62.2172 65.2546 59.7876 53.3788 49.0933C52.558 48.3541 52.5589 47.0812 53.34 46.3001L59.6444 39.9957C60.4255 39.2147 61.6876 39.2218 62.5289 39.9376C78.3822 53.4271 110.419 46.8282 127.107 31.5825C127.923 30.8375 129.185 30.8353 129.966 31.6163L130.081 31.731C130.09 31.7397 130.099 31.7486 130.108 31.7575L136.613 38.2629C137.394 39.044 137.392 40.3065 136.647 41.1219C121.396 57.8086 114.736 89.791 128.292 105.701C129.008 106.542 129.015 107.804 128.234 108.585L121.93 114.889C121.149 115.67 119.876 115.671 119.136 114.85C108.442 102.975 106.013 85.3602 109.316 68.3885L40.4466 137.258C39.6655 138.039 38.3992 138.039 37.6181 137.258L30.9713 130.611C30.1903 129.83 30.1903 128.564 30.9713 127.783L99.841 58.9134Z"
                                 fill="#0B0E11"
                             />
                         </svg>
@@ -107,16 +110,61 @@ export default {
     border-top: 1px solid c('base-7');
     z-index: 2;
 
+    .container {
+    }
+
+    &-magic {
+        position: absolute;
+        top: -100%;
+        left: 0;
+        width: 100%;
+        height: 200%;
+        z-index: -1;
+        pointer-events: none;
+        background: radial-gradient(ellipse at 10% 90%, c('base-0'), transparent 60%);
+        opacity: 0.2;
+        filter: blur(var(--size));
+        transition: background 0.66s cubic-bezier(0.075, 0.82, 0.165, 1);
+
+        html[theme='dark'] & {
+            background: radial-gradient(ellipse at 10% 90%, c('base-3'), transparent 60%);
+        }
+    }
+
     &-links {
         display: flex;
         align-items: center;
         gap: 2em;
 
         a {
-            display: flex;
+            position: relative;
+            display: inline-flex;
             align-items: center;
+            color: c('base-0');
 
-            span {
+            &:after {
+                content: '';
+                position: absolute;
+                bottom: 12%;
+                left: -0.125em;
+                right: -0.125em;
+                height: 0.35em;
+                background: c('primary-base');
+                transform: scaleX(0);
+                opacity: 0;
+                transform-origin: 0 50%;
+                transition: transform 0.66s cubic-bezier(0.42, 0.39, 0.17, 1), opacity 0.66s ease;
+                z-index: -1;
+            }
+            &:hover,
+            &:focus {
+                color: c('base-0');
+
+                &::after {
+                    transform: scaleX(0.73);
+                    opacity: 0.5;
+                    transition: transform 0.92s cubic-bezier(0.42, 0.39, 0.17, 1);
+                }
             }
 
             svg {
@@ -127,7 +175,7 @@ export default {
                 margin-bottom: 0.25em;
 
                 path {
-                    fill: c('base-4');
+                    fill: c('base-0');
                 }
 
                 &.-tiny {
