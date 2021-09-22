753d7e6559da7cafaa229f5f6a5a68a55531de89
Kolby Kruger <kolbykruger@gmail.com>
2021-09-22 18:40:07 -0400

    created Accessibility component

---
+++ components/Accessibility/Accessibility.vue
@@ -0,0 +1,29 @@
+<template>
+    <div class="accessibility">
+        <button @click="setFocus('navigation')">Skip to: Navigation</button>
+        <button @click="setFocus('content')">Skip to: Content</button>
+    </div>
+</template>
+
+<script>
+export default {
+    computed: {
+        path() {
+            return this.$route.path
+        }
+    },
+    methods: {
+        setFocus(elem) {
+            this.$nextTick(() => {
+                document.getElementById('navigation').focus()
+            })
+        }
+    }
+}
+</script>
+
+<style lang="scss">
+.accessibility {
+    display: none;
+}
+</style>
