f2d6dff93cd577a35c4f342a3daf45d7ec1f591f
Kolby Kruger <kolbykruger@gmail.com>
2021-09-23 16:24:51 -0400

    moved router scroll-behavior to plugins

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,4 +1,5 @@
 import smConfig from './sm.json'
+import { scrollBehavior } from './plugins/route.js'
 
 if (!smConfig.apiEndpoint) {
     console.warn(
@@ -55,12 +56,13 @@ export default {
     // Auto import components (https://go.nuxtjs.dev/config-components)
     components: true,
     // Modules for dev and build (recommended) (https://go.nuxtjs.dev/config-modules)
-    buildModules: ['@nuxt/image', '@nuxtjs/style-resources'],
+    buildModules: ['@nuxt/image'],
     // Transpile
     transpile: ['gsap', 'three'],
     // Modules (https://go.nuxtjs.dev/config-modules)
     modules: [
         '@nuxt/image',
+        '@nuxtjs/style-resources',
         [
             '@nuxtjs/prismic',
             {
@@ -115,35 +117,6 @@ export default {
     },
     //scrollToTop: true,
     router: {
-        scrollBehavior: async (to, from, savedPosition) => {
-            if (savedPosition) {
-                return savedPosition
-            }
-
-            const findEl = async (hash, x) => {
-                return (
-                    document.querySelector(hash) ||
-                    new Promise((resolve, reject) => {
-                        if (x > 50) {
-                            return resolve()
-                        }
-                        setTimeout(() => {
-                            resolve(findEl(hash, ++x || 1))
-                        }, 100)
-                    })
-                )
-            }
-
-            if (to.hash) {
-                let el = await findEl(to.hash)
-                if ('scrollBehavior' in document.documentElement.style) {
-                    return window.scrollTo({ top: el.offsetTop, behavior: 'smooth' })
-                } else {
-                    return window.scrollTo(0, el.offsetTop)
-                }
-            }
-
-            return { x: 0, y: 0 }
-        }
+        scrollBehavior
     }
 }
