08640484761bd99cf4423aa7cada594455845302
Kolby Kruger <kolbykruger@gmail.com>
2021-09-23 16:26:19 -0400

    added functionality for accessible navigation

--- components/Accessibility/Accessibility.vue
+++ components/Accessibility/Accessibility.vue
@@ -1,8 +1,10 @@
 <template>
-    <div class="accessibility">
-        <button @click="setFocus('navigation')">Skip to: Navigation</button>
-        <button @click="setFocus('content')">Skip to: Content</button>
-    </div>
+    <section class="accessibility">
+        <div class="container">
+            <button @click="setFocus('navigation')">Skip to Navigation</button>
+            <button @click="setFocus('content')">Skip to Content</button>
+        </div>
+    </section>
 </template>
 
 <script>
@@ -13,9 +15,18 @@ export default {
         }
     },
     methods: {
-        setFocus(elem) {
+        setFocus(el) {
             this.$nextTick(() => {
-                document.getElementById('navigation').focus()
+                const elem = document.getElementById(el)
+
+                if (elem) {
+                    elem.tabIndex = -1
+                    elem.scrollTo({
+                        top: elem.offsetTop,
+                        behavior: 'smooth'
+                    })
+                    elem.focus()
+                }
             })
         }
     }
@@ -24,6 +35,35 @@ export default {
 
 <style lang="scss">
 .accessibility {
-    display: none;
+    position: absolute;
+    top: 0;
+    left: 0;
+    width: 100%;
+    z-index: 9999;
+    transform: translateY(-100%);
+
+    &:focus,
+    &:focus-within {
+        transform: translateY(0%);
+    }
+
+    .container {
+        display: flex;
+        align-items: center;
+        justify-content: center;
+        padding-top: 1em;
+        padding-bottom: 1em;
+    }
+
+    button {
+        font-size: 1.5rem;
+        border-radius: 0.35em;
+        margin-right: 0.5em;
+        line-height: 1;
+        height: 52px;
+        padding: 0 0.65em;
+        background: c('base-3');
+        color: c('base-9');
+    }
 }
 </style>
