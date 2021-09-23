324e8aa6cea13bb30a3e7a23a99d7e127da668b4
Kolby Kruger <kolbykruger@gmail.com>
2021-09-23 16:32:33 -0400

    added pageheading animation

--- components/Pageheading/Pageheading.vue
+++ components/Pageheading/Pageheading.vue
@@ -1,7 +1,10 @@
 <template>
     <section class="pageheading">
         <div class="container" :data-type="type">
-            <prismic-rich-text class="pageheading-name" v-if="typeof name == 'object'" :field="name" />
+            <div class="pageheading-name" v-if="typeof name == 'object'">
+                <h1>{{ name[0].text }}</h1>
+            </div>
+
             <div class="pageheading-name" v-else>
                 <h1>{{ name || 'Pageheading' }}</h1>
             </div>
@@ -10,6 +13,8 @@
 </template>
 
 <script>
+import { gsap } from 'gsap'
+
 export default {
     name: 'pageheading',
     props: {
@@ -33,6 +38,25 @@ export default {
 
     .container {
         padding-top: 10vh;
+    }
+
+    &-name {
+        clip-path: polygon(-100% 0, 200% 0, 200% 100%, -100% 100%);
+
+        h1 {
+            transform: translateY(150%);
+            animation: pageheading-in 0.92s cubic-bezier(0.23, 1, 0.32, 1) forwards;
+            animation-delay: 0.12s;
+
+            @keyframes pageheading-in {
+                0% {
+                    transform: translate(0%, 150%) rotate(5deg);
+                }
+                100% {
+                    transform: translate(0%, 0%) rotate(0deg);
+                }
+            }
+        }
     }
 }
 </style>
