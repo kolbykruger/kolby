d32f87b4e3459a98366f098a01078da4ca4faeeb
Kolby Kruger <kolbykruger@gmail.com>
2021-11-06 13:18:45 -0400

    added intro back in

---
+++ components/Introduction/Introduction.vue
@@ -0,0 +1,56 @@
+<template>
+    <section class="introduction">
+        <div class="container">
+            <h1 class="introduction-title">Kolby Kruger</h1>
+            <h2 class="introduction-subtitle">Web designer & developer<br />based in Vermont.</h2>
+            <p class="introduction-description">
+                I have a passion for design, programming, motion animation, and web apps.
+            </p>
+        </div>
+        <!-- <canvas ref="canvas"></canvas> -->
+    </section>
+</template>
+
+<script>
+//import { Canvas } from '~/mixins/intro/Canvas.js'
+export default {
+    name: 'Introduction',
+    //mixins: [Canvas]
+}
+</script>
+
+<style lang="scss">
+.introduction {
+    position: relative;
+    margin-top: calc(var(--offset) * -1);
+    display: grid;
+    align-items: center;
+    justify-content: flex-start;
+    min-height: 100vh;
+    overflow: hidden;
+
+    .container {
+        position: absolute;
+        top: 0;
+        left: 0;
+        width: 100%;
+        height: 100%;
+        position: relative;
+        display: grid;
+        place-content: center;
+        z-index: 4;
+    }
+    canvas {
+        z-index: 2;
+        filter: blur(50px);
+        opacity: 0.7;
+    }
+    &-title {
+    }
+    &-subtitle {
+    }
+    &-description {
+        max-width: 18em;
+    }
+}
+</style>
