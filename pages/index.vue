56105b7ec1ee0f0f07c2f9233c4e3cf0acbf4283
Kolby Kruger <kolbykruger@gmail.com>
2021-11-06 13:19:28 -0400

    added introduction back in

--- pages/index.vue
+++ pages/index.vue
@@ -1,20 +1,20 @@
 <template>
     <main class="page" id="content">
         <div class="page-contents">
-            <!-- <Introduction /> -->
+            <Introduction />
             <!-- <slice-zone type="page" uid="home" /> -->
         </div>
     </main>
 </template>
 
 <script>
-// import Introduction from '@/components/Introduction/Introduction.vue'
+import Introduction from '@/components/Introduction/Introduction.vue'
 import { Animations } from '~/mixins/animations/Animations.js'
 // import SliceZone from 'vue-slicezone'
 
 export default {
     components: {
-        // Introduction
+        Introduction,
         // SliceZone
     },
     mixins: [Animations],
