d9069a7759de09b2a6bafedc01709eee44a6cf72
Kolby Kruger <kolbykruger@gmail.com>
2021-11-08 14:04:49 -0500

    integrated curated featured projects

--- pages/index.vue
+++ pages/index.vue
@@ -2,6 +2,20 @@
     <main class="page" id="content">
         <div class="page-contents">
             <Introduction />
+
+            <section class="headline">
+                <div class="container">
+                    <h2>Featured <br />work</h2>
+                </div>
+            </section>
+
+            <section class="case-studies" v-if="cases">
+                <div class="container">
+                    <CaseStudyItem v-for="study in cases.results" :key="study.id" :item="study" />
+                    <nuxt-link to="/work">View more work</nuxt-link>
+                </div>
+            </section>
+
             <!-- <slice-zone type="page" uid="home" /> -->
         </div>
     </main>
@@ -15,8 +29,24 @@ import { Animations } from '~/mixins/animations/Animations.js'
 export default {
     components: {
         Introduction,
+        // Headline,
         // SliceZone
     },
     mixins: [Animations],
+    async asyncData({ $prismic, params, error }) {
+        const document = await $prismic.api.query($prismic.predicates.at('document.type', 'home'))
+
+        const caseStudyIds = document.results[0].data.FeaturedWork.map(i => {
+            return i.CaseStudy.id
+        })
+
+        const cases = await $prismic.api.getByIDs(caseStudyIds)
+
+        if (cases && document) {
+            return { document, cases }
+        } else {
+            error({ statusCode: 404, message: 'Page not found' })
+        }
+    },
 }
 </script>
