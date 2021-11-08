39bb7e596172f851c4725706ffe034b9948c505a
Kolby Kruger <kolbykruger@gmail.com>
2021-11-08 14:02:44 -0500

    generated mocks and models

--- .slicemachine/assets/slices/Headline/index.stories.js
+++ .slicemachine/assets/slices/Headline/index.stories.js
@@ -18,7 +18,7 @@ export const _DefaultSlice = () => ({
   },
   data() {
     return {
-      mock: {"variation":"default-slice","name":"Default slice","slice_type":"headline","items":[],"primary":{"title":[{"type":"heading1","text":"Embrace strategic interfaces","spans":[]}]},"id":"_DefaultSlice"}
+      mock: {"variation":"default-slice","name":"Default slice","slice_type":"headline","items":[],"primary":{"title":[{"type":"heading1","text":"Reintermediate user-centric platforms","spans":[]}]},"id":"_DefaultSlice"}
     }
   },
   template: '<SliceZone :slices="[mock]" :resolver="resolve" />'
--- .slicemachine/assets/slices/Timeline/index.stories.js
+++ .slicemachine/assets/slices/Timeline/index.stories.js
@@ -18,7 +18,7 @@ export const _DefaultSlice = () => ({
   },
   data() {
     return {
-      mock: {"variation":"default-slice","name":"Default slice","slice_type":"timeline","items":[{"Eyebrow":"leverage plug-and-play supply-chains","Title":"implement impactful channels","Summary":[{"type":"paragraph","text":"Voluptate consectetur consequat veniam in consequat cillum pariatur aute aliquip et. Amet laborum adipisicing reprehenderit occaecat cupidatat aute eu nostrud mollit mollit tempor cupidatat ad. Esse duis incididunt consequat Lorem do.","spans":[]}]},{"Eyebrow":"facilitate collaborative methodologies","Title":"seize out-of-the-box platforms","Summary":[{"type":"paragraph","text":"Occaecat velit fugiat aute magna aliquip. Dolor reprehenderit do incididunt anim laboris incididunt duis ex excepteur et sint ex aliqua sint ea.","spans":[]}]},{"Eyebrow":"target enterprise infomediaries","Title":"reintermediate vertical e-business","Summary":[{"type":"paragraph","text":"Laborum deserunt Lorem esse cillum dolore adipisicing.","spans":[]}]},{"Eyebrow":"synthesize extensible networks","Title":"redefine collaborative content","Summary":[{"type":"paragraph","text":"Id minim nisi elit ipsum tempor. Est pariatur consequat adipisicing consectetur voluptate ex excepteur.","spans":[]}]}],"primary":{},"id":"_DefaultSlice"}
+      mock: {"variation":"default-slice","name":"Default slice","slice_type":"timeline","items":[{"Eyebrow":"engineer back-end e-business","Title":"enhance cross-platform networks","Summary":[{"type":"paragraph","text":"Excepteur et sit quis veniam consectetur minim laboris fugiat laboris irure est non magna deserunt.","spans":[]}]},{"Eyebrow":"optimize front-end experiences","Title":"transition out-of-the-box e-tailers","Summary":[{"type":"paragraph","text":"Amet laborum ullamco culpa velit non ex. Duis anim veniam ea adipisicing. Deserunt laborum aliquip magna officia minim ad quis exercitation dolor.","spans":[]}]},{"Eyebrow":"utilize sticky models","Title":"embrace user-centric experiences","Summary":[{"type":"paragraph","text":"Occaecat quis officia sunt anim cillum duis.","spans":[]}]},{"Eyebrow":"engage enterprise paradigms","Title":"cultivate dot-com interfaces","Summary":[{"type":"paragraph","text":"Dolore duis laboris sunt occaecat occaecat mollit consequat dolore elit. Deserunt reprehenderit dolore nulla pariatur magna aliqua ex enim eu velit duis. Velit cupidatat anim aute elit voluptate minim proident duis labore ut officia do.","spans":[]}]}],"primary":{},"id":"_DefaultSlice"}
     }
   },
   template: '<SliceZone :slices="[mock]" :resolver="resolve" />'
@@ -37,9 +37,28 @@ export const _TimelineVertical = () => ({
   },
   data() {
     return {
-      mock: {"variation":"timelineVertical","name":"Timeline - Vertical","slice_type":"timeline","items":[{"Eyebrow":"synergize back-end initiatives","Title":"repurpose 24/7 applications","Summary":[{"type":"paragraph","text":"Mollit velit mollit nulla sint quis do irure consectetur anim commodo adipisicing magna amet ea proident.","spans":[]}]},{"Eyebrow":"matrix turn-key deliverables","Title":"architect granular e-commerce","Summary":[{"type":"paragraph","text":"Ad excepteur commodo velit qui ad enim anim aliquip cupidatat reprehenderit do culpa fugiat quis. Eiusmod incididunt id tempor deserunt excepteur ea excepteur culpa et. Qui veniam sint occaecat est irure nostrud veniam eiusmod culpa quis eu culpa et.","spans":[]}]}],"primary":{},"id":"_TimelineVertical"}
+      mock: {"variation":"timelineVertical","name":"Timeline - Vertical","slice_type":"timeline","items":[{"Eyebrow":"matrix turn-key metrics","Title":"unleash rich metrics","Summary":[{"type":"paragraph","text":"Aliqua fugiat elit laboris qui voluptate aute ullamco qui velit mollit duis.","spans":[]}]},{"Eyebrow":"generate magnetic channels","Title":"optimize visionary applications","Summary":[{"type":"paragraph","text":"Ut cupidatat consectetur ullamco non nulla anim veniam culpa amet esse. Duis reprehenderit amet dolor ullamco tempor sunt velit culpa.","spans":[]}]},{"Eyebrow":"incentivize rich infrastructures","Title":"synthesize compelling users","Summary":[{"type":"paragraph","text":"Id commodo excepteur Lorem aliqua cillum aliquip do amet irure nulla cupidatat consectetur ut deserunt. Non officia sint irure elit.","spans":[]}]},{"Eyebrow":"engineer global bandwidth","Title":"synergize clicks-and-mortar architectures","Summary":[{"type":"paragraph","text":"Minim eu nostrud excepteur et ad ea incididunt nulla cillum ipsum consequat dolore veniam excepteur do.","spans":[]}]}],"primary":{},"id":"_TimelineVertical"}
     }
   },
   template: '<SliceZone :slices="[mock]" :resolver="resolve" />'
 })
 _TimelineVertical.storyName = 'Timeline - Vertical'
+
+export const _TimelineImages = () => ({
+  components: {
+    MyComponent,
+    SliceZone
+  },
+  methods: {
+    resolve() {
+      return MyComponent
+    }
+  },
+  data() {
+    return {
+      mock: {"variation":"timelineImages","name":"Timeline - Images","slice_type":"timeline","items":[{"Eyebrow":"matrix seamless supply-chains","Image":{"dimensions":{"width":1066,"height":600},"alt":"Placeholder image","copyright":null,"url":"https://images.unsplash.com/photo-1589321599763-d66926c29613?w=1066&h=600&fit=crop"},"Title":"deliver visionary niches","Summary":[{"type":"paragraph","text":"Sint labore sunt ipsum pariatur velit do sint nulla tempor. Qui velit sit do sit ea in anim. Et minim veniam anim magna amet.","spans":[]}]},{"Eyebrow":"embrace magnetic metrics","Image":{"dimensions":{"width":1066,"height":600},"alt":"Placeholder image","copyright":null,"url":"https://images.unsplash.com/photo-1587905069134-008460d7a636?w=1066&h=600&fit=crop"},"Title":"envisioneer real-time technologies","Summary":[{"type":"paragraph","text":"Sunt commodo elit consectetur sint commodo reprehenderit sunt eu amet. Laboris magna non in irure aliquip incididunt deserunt nisi ut nulla qui aliquip incididunt nisi esse. Nostrud deserunt est sint anim pariatur laboris voluptate est proident id aliqua pariatur aliqua cupidatat.","spans":[]}]}],"primary":{},"id":"_TimelineImages"}
+    }
+  },
+  template: '<SliceZone :slices="[mock]" :resolver="resolve" />'
+})
+_TimelineImages.storyName = 'Timeline - Images'
