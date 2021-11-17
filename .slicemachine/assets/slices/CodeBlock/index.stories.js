5123185c5145bfbd4485163d86697059070afff9
Kolby Kruger <kolbykruger@gmail.com>
2021-11-17 18:14:00 -0500

    updated mocks for codeblock

--- .slicemachine/assets/slices/CodeBlock/index.stories.js
+++ .slicemachine/assets/slices/CodeBlock/index.stories.js
@@ -1,26 +1,41 @@
-import MyComponent from '../../../../slices/CodeBlock';
+import MyComponent from '../../../../slices/CodeBlock'
 import SliceZone from 'vue-slicezone'
 
 export default {
-  title: 'slices/CodeBlock'
+    title: 'slices/CodeBlock',
 }
 
-
 export const _DefaultSlice = () => ({
-  components: {
-    MyComponent,
-    SliceZone
-  },
-  methods: {
-    resolve() {
-      return MyComponent
-    }
-  },
-  data() {
-    return {
-      mock: {"variation":"default-slice","name":"Default slice","slice_type":"code_block","items":[],"primary":{"Language":"html","File":"incubate scalable e-business","Code":[{"type":"paragraph","text":"Ex esse duis non aliqua ex adipisicing quis elit dolor magna cillum eu. Occaecat sint veniam culpa. Labore mollit culpa deserunt pariatur incididunt incididunt duis eiusmod voluptate incididunt enim do.","spans":[]}]},"id":"_DefaultSlice"}
-    }
-  },
-  template: '<SliceZone :slices="[mock]" :resolver="resolve" />'
+    components: {
+        MyComponent,
+        SliceZone,
+    },
+    methods: {
+        resolve() {
+            return MyComponent
+        },
+    },
+    data() {
+        return {
+            mock: {
+                variation: 'default-slice',
+                name: 'Default slice',
+                slice_type: 'code_block',
+                items: [],
+                primary: {
+                    Language: 'css',
+                    File: 'index.js',
+                    Code: [
+                        {
+                            type: 'preformatted',
+                            text: 'div {\n  display: flex;\n  color: red;\n  background: green;\n}',
+                            spans: [],
+                        },
+                    ],
+                },
+            },
+        }
+    },
+    template: '<SliceZone :slices="[mock]" :resolver="resolve" />',
 })
 _DefaultSlice.storyName = 'Default slice'
