e51bfee01df86dcabdd6ef01f36c3ce40f930f0f
Kolby Kruger <kolbykruger@gmail.com>
2021-11-17 01:14:41 -0500

    added a scroll button

--- components/Introduction/Introduction.vue
+++ components/Introduction/Introduction.vue
@@ -1,5 +1,5 @@
 <template>
-    <section class="introduction">
+    <section class="introduction" ref="intro">
         <div class="container">
             <h1 ref="title" class="introduction-title">Kolby Kruger</h1>
             <h2 ref="subTitle" class="introduction-subtitle">I design and create web <b>experiences</b></h2>
@@ -7,6 +7,25 @@
                 I'm a designer & developer based in Vermont that is passionate about creating extraordinary online
                 experiences through web design, web development, and ux/ui.
             </p>
+            <button
+                ref="button"
+                @click="scrollNext"
+                data-stick
+                data-progress="6"
+                data-callback="intro"
+                class="introduction-indicator"
+            >
+                <svg viewBox="0 0 97 67" xmlns="http://www.w3.org/2000/svg">
+                    <path
+                        d="m1.9986 12.996c-1.1011-0.088-1.9986-0.9754-1.9986-2.08l-6.7312e-7 -8.9158c-8.3393e-8 -1.1046 0.89943-2.0053 2.0025-1.9476 28.898 1.5128 51.94 35.49 52.962 64.518 0.0389 1.1039-0.86 1.9959-1.9646 1.9959h-9.2c-1.1046 0-1.9958-0.8943-2.0454-1.9978-1.0151-22.584-18.921-49.908-39.756-51.572z"
+                        fill="#0B0E11"
+                    />
+                    <path
+                        d="m44 66.566c-1.1046 0-2.0035-0.892-1.9646-1.9959 1.0222-29.027 24.064-63.005 52.962-64.518 1.1031-0.057744 2.0025 0.84303 2.0025 1.9476v8.9158c0 1.1046-0.8975 1.992-1.9985 2.0807-20.748 1.6715-38.736 28.991-39.756 51.572-0.0498 1.1035-0.941 1.9978-2.0456 1.9978h-9.2z"
+                        fill="#0B0E11"
+                    />
+                </svg>
+            </button>
         </div>
     </section>
 </template>
@@ -18,6 +37,19 @@ import { CustomEase } from 'gsap/dist/CustomEase'
 
 export default {
     name: 'Introduction',
+    methods: {
+        scrollNext() {
+            const next = this.$refs.intro.nextElementSibling.childNodes[0]
+            const nextBounds = next.getBoundingClientRect()
+            const nextOffset = nextBounds.top
+            const nextHeight = (nextBounds.height - window.innerHeight) / 2
+            const scrollPos = nextOffset + nextHeight
+            window.scrollTo({
+                top: scrollPos,
+                behavior: 'smooth',
+            })
+        },
+    },
     mounted() {
         gsap.registerPlugin(SplitText)
         gsap.registerPlugin(CustomEase)
@@ -128,6 +160,22 @@ export default {
 
     &-description {
         max-width: 25em;
+    }
+
+    &-indicator {
+        position: absolute;
+        bottom: 6vh;
+        --size: 3em;
+        width: var(--size);
+        height: var(--size);
+        padding: 1em;
+        border-radius: 50%;
+
+        svg {
+            path {
+                fill: c('base-0');
+            }
+        }
     }
 }
 </style>
