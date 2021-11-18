559cc39ef7deaad8868389bac50ab43321169a2b
Kolby Kruger <kolbykruger@gmail.com>
2021-11-18 00:55:48 -0500

    added anchors to heading3

--- mixins/serializer/Serializer.js
+++ mixins/serializer/Serializer.js
@@ -4,8 +4,12 @@ export const Serializer = {
         htmlSerializer(type, element, content, children) {
             if (type == 'heading2') {
                 const id = element.text.replace(/\W+/g, '-').toLowerCase()
-                return `<a class="anchor-link" name="${id}"></a><h2>${children.join('')}</h2>`
+                return `<a class="anchor-link" name="${id}" data-type="h2"></a><h2>${children.join('')}</h2>`
             }
-        }
-    }
+            if (type == 'heading3') {
+                const id = element.text.replace(/\W+/g, '-').toLowerCase()
+                return `<a class="anchor-link" name="${id}" data-type="h3"></a><h3>${children.join('')}</h3>`
+            }
+        },
+    },
 }
