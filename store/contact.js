9f0a1b0f1b0456d8b4c7c934bd9773b09dd0d54c
Kolby Kruger <kolbykruger@gmail.com>
2021-11-29 13:01:41 -0500

    added global store for contact

---
+++ store/contact.js
@@ -0,0 +1,10 @@
+export const state = () => ({
+    email: 'hi@kolby.dev',
+    phone: '(802) 999-8273',
+    socials: {
+        github: 'https://github.com/kolbykruger',
+        linkedin: 'https://www.linkedin.com/in/kolby-kruger/',
+        figma: 'https://www.figma.com/@kolbykruger',
+        spotify: 'https://open.spotify.com/user/y06tzx3ihl845bexod52qrric?si=99779530345d4977',
+    },
+})
