f27b1e6d46c7f24db00de41850b350197a359850
Kolby Kruger <kolbykruger@gmail.com>
2021-11-29 00:37:55 -0500

    added dates + re-styled

--- components/ArticleItem/ArticleItem.vue
+++ components/ArticleItem/ArticleItem.vue
@@ -1,24 +1,15 @@
 <template>
     <li class="article-item">
         <nuxt-link data-cursor="lg" :to="'/article/' + article.uid">
-            <!-- <p class="article-item-category">Self</p> -->
-            <h2 class="article-item-title">{{ article.data.Name[0].text }}</h2>
-
-            <prismic-rich-text class="article-item-summary" :field="article.data.Summary" />
-
-            <!-- <div class="article-item-details">
+            <div class="article-item-details">
                 <p class="article-item-date">
                     <small>{{ formatDate }}</small>
                 </p>
-                <span v-if="article.data.Read"> — </span>
-                <p class="article-item-read" v-if="article.data.Read">
-                    <small>{{ article.data.Read }} min read</small>
-                </p>
-                <span v-if="article.data.Category"> — </span>
-                <p class="article-item-category" v-if="article.data.Category">
-                    <small>{{ article.data.Category }}</small>
-                </p>
-            </div> -->
+            </div>
+
+            <h2 class="article-item-title">{{ article.data.Name[0].text }}</h2>
+
+            <!-- <prismic-rich-text class="article-item-summary" :field="article.data.Summary" /> -->
         </nuxt-link>
     </li>
 </template>
@@ -46,8 +37,9 @@ export default {
 <style lang="scss">
 .article {
     &-item {
-        max-width: 800px;
-        margin-bottom: 10vh;
+        margin-bottom: 3vh;
+        padding-bottom: 3vh;
+        border-bottom: 1px solid c('base-4');
 
         a {
             display: inline-block;
@@ -66,9 +58,10 @@ export default {
         }
 
         &-title {
+            font-family: $font-0;
             font-size: clamp(2.441rem, -0.875rem + 8.333vw, 3.052rem);
             margin: 0;
-            margin-bottom: 0.5em;
+            margin-bottom: 0;
             transition: color 0.66s cubic-bezier(0.075, 0.82, 0.165, 1);
         }
 
