3d51b5ed3df3381f27451318fd61a06baac40bc1
Kolby Kruger <kolbykruger@gmail.com>
2021-12-06 18:59:23 -0500

    minor refactor to include in storybook

--- components/Menu/Menu.stories.js
+++ components/Menu/Menu.stories.js
@@ -1,5 +1,21 @@
-export default {
-    title: 'Core/Menu'
-}
+// export default {
+//     title: 'Core/Menu',
+// }
 
-export const Default = () => '<Menu />'
+// export const Default = () => '<Menu />'
+
+// // const Template = (args, { argTypes }) => {
+// //     props: Object.keys(argTypes)
+// // }
+
+// // export const Primary = template.bind({})
+
+// // Primary.args = {
+// //     primary: true,
+// // }
+
+import Menu from './Menu.vue'
+export const Default = {
+    title: 'Core/Menu',
+    component: Menu,
+}
