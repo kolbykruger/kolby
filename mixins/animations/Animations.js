85c118a4ad3a1210b229d06a86c6de636a95edbb
Kolby Kruger <kolbykruger@gmail.com>
2021-12-06 18:55:55 -0500

    refactored animations

--- mixins/animations/Animations.js
+++ mixins/animations/Animations.js
@@ -1,26 +1,30 @@
 import { gsap } from 'gsap'
 import { ScrollTrigger } from 'gsap/dist/ScrollTrigger'
+import { CustomEase } from 'gsap/dist/CustomEase'
 
 export const Animations = {
     name: 'Animations',
     data() {
         return {
+            timeline: null,
             animations: null,
-            elems: null
+            elems: null,
+            ease: null,
         }
     },
     methods: {
         initAnimations() {
             this.resetAnimations()
-            this.$nextTick(() => {
-                setTimeout(() => {
-                    this.getAnimations()
-                }, 1100)
+            this.$router.onReady(() => {
+                this.$nextTick(() => {
+                    setTimeout(() => {
+                        this.getAnimations()
+                    }, 1000)
+                })
             })
         },
         getAnimations() {
             const sections = document.querySelectorAll('[data-anim]')
-            console.log(sections)
 
             if (!sections) {
                 return false
@@ -34,26 +38,47 @@ export const Animations = {
             this.createAnimations()
         },
         createAnimations() {
-            this.animations = gsap.utils.toArray(this.elems).map(el => {
-                return gsap.from(el, {
-                    scrollTrigger: {
-                        trigger: el,
-                        start: 'top bottom',
-                        markers: false,
-                        animation: this.tl,
-                        onEnter: () => {
-                            el.classList.add('anim--active')
-                            // gsap.to(el, 0.95, {
-                            //     y: '0%',
-                            //     rotateX: 0,
-                            //     d: 0,
-                            //     opacity: 1,
-                            //     ease: 'Power2.easeOut',
-                            //     delay: 1.16
-                            // })
-                        }
-                    }
-                })
+            this.timeline = gsap.timeline()
+            this.animations = gsap.utils.toArray(this.elems).map((el, i) => {
+                // Props
+                // Start
+                const start = el.getAttribute('data-anim-start') || '80%'
+                // Duration
+                const duration = el.getAttribute('data-anim-duration') || 0.95
+                // Elements
+                let targets = el
+                const elements = el.getAttribute('data-anim-elements')
+                if (elements != '' && elements && elements != null) {
+                    targets = elements
+                }
+                // Stagger
+                const stagger = el.getAttribute('data-anim-stagger') || 0.2
+
+                return this.timeline
+                    .set(targets, {
+                        y: '12%',
+                        opacity: 0,
+                    })
+                    .to(targets, duration, {
+                        scrollTrigger: {
+                            trigger: el,
+                            start: 'top ' + start,
+                            toggleActions: 'play none none none',
+                            markers: false,
+                            animation: this.tl,
+                            onEnter: () => {
+                                el.classList.add('anim--active')
+
+                                gsap.to(targets, duration, {
+                                    y: '0%',
+                                    opacity: 1,
+                                    ease: this.ease,
+                                    duration: duration,
+                                    stagger: targets.length > 1 ? stagger : 0,
+                                })
+                            },
+                        },
+                    })
             })
         },
         resetAnimations() {
@@ -70,9 +95,11 @@ export const Animations = {
                     anim.kill()
                 }
             })
-        }
+        },
     },
     mounted() {
+        gsap.registerPlugin(CustomEase)
+        this.ease = CustomEase.create('custom', 'M0,0 C0.23,1 0.32,1 1,1 ')
         this.initAnimations()
     },
     updated() {
@@ -86,6 +113,6 @@ export const Animations = {
             if (!value) {
                 this.initTraveler()
             }
-        }
-    }
+        },
+    },
 }
