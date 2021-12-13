6333c55c982ae4ae3ca0866805c7f65729e16145
Kolby Kruger <kolbykruger@gmail.com>
2021-12-13 11:34:45 -0500

    reworked accordion component to support child components

--- slices/Accordion/index.vue
+++ slices/Accordion/index.vue
@@ -1,45 +1,24 @@
 <template>
-    <section class="accordion" data-anim>
+    <section class="accordion" data-anim=".accordion-item">
         <div class="container">
             <div class="accordion-heading" v-if="slice.primary.heading">
                 <prismic-rich-text :field="slice.primary.heading" />
             </div>
             <div class="accordion-items">
-                <div class="accordion-item" v-for="(item, i) in slice.items" :key="`slice-item-${i}`" data-anim>
-                    <a :name="encodeURIComponent(item.Title)"></a>
-                    <button class="accordion-button" @click="toggle">
-                        <p class="accordion-button-title">
-                            {{ item.Title }}
-                        </p>
-                        <div class="accordion-button-icon" data-anchor data-exclusion>
-                            <svg
-                                xmlns="http://www.w3.org/2000/svg"
-                                width="97"
-                                height="141"
-                                viewBox="0 0 97 141"
-                                fill="none"
-                            >
-                                <path
-                                    fill-rule="evenodd"
-                                    clip-rule="evenodd"
-                                    d="M55.1999 100.055C64.8646 85.7176 79.0381 74.9801 94.9975 74.1447 96.1006 74.0869 97 74.9877 97 76.0923V85.0081C97 86.1126 96.1024 87.0001 95.0014 87.0888 74.253 88.7602 56.2656 116.08 55.2456 138.661 55.1958 139.764 54.3046 140.658 53.2 140.658H53 44 43.8C42.6954 140.658 41.8042 139.764 41.7546 138.66 40.7395 116.077 22.8338 88.7529 1.99862 87.0881.897561 87.0001-694524e-11 86.1126-689696e-11 85.0081L-650723e-11 76.0923C-645895e-11 74.9877.899431 74.0869 2.00249 74.1447 17.9619 74.9801 32.1353 85.7175 41.7999 100.054L41.8 2.6582C41.8 1.55363 42.6954.658201 43.8.658201H53.1999C54.3045.658201 55.1999 1.55363 55.1999 2.6582V100.055z"
-                                    fill="#0b0e11"
-                                />
-                            </svg>
-                        </div>
-                    </button>
-                    <div class="accordion-panel" aria-expanded="false" aria-hidden="true">
-                        <prismic-rich-text :field="item.Description" />
-                    </div>
-                </div>
+                <Item v-for="(item, index) in slice.items" :item="item" :index="index" :key="index" />
             </div>
         </div>
     </section>
 </template>
 
 <script>
+import Item from './item.vue'
+
 export default {
     name: 'Accordion',
+    components: {
+        Item,
+    },
     props: {
         slice: {
             type: Object,
@@ -56,19 +35,19 @@ export default {
             const item = event.currentTarget.parentElement
 
             //add styling/classes/attributes to parent
-            item.classList.contains('accordion-item--expanded')
-                ? item.classList.remove('accordion-item--expanded')
-                : item.classList.add('accordion-item--expanded')
+            item.classList.contains('.-expanded')
+                ? item.classList.remove('.-expanded')
+                : item.classList.add('.-expanded')
 
             //Add styling/classes/attributes to button
-            button.classList.contains('accordion-button--expanded')
-                ? button.classList.remove('accordion-button--expanded')
-                : button.classList.add('accordion-button--expanded')
+            button.classList.contains('.-expanded')
+                ? button.classList.remove('.-expanded')
+                : button.classList.add('.-expanded')
 
             //Add styling/classes/attributes to panel
-            panel.classList.contains('accordion-panel--expanded')
-                ? panel.classList.remove('accordion-panel--expanded')
-                : panel.classList.add('accordion-panel--expanded')
+            panel.classList.contains('.-expanded')
+                ? panel.classList.remove('.-expanded')
+                : panel.classList.add('.-expanded')
             panel.style.maxHeight = panel.style.maxHeight ? null : panel.scrollHeight + 'px'
             panel.setAttribute('aria-expanded', panel.style.maxHeight ? true : false)
             panel.setAttribute('aria-hidden', panel.style.maxHeight ? false : true)
@@ -81,7 +60,7 @@ export default {
 }
 </script>
 
-<style lang="scss" scoped>
+<style lang="scss">
 .accordion {
     .container {
         max-width: 92em;
@@ -89,69 +68,18 @@ export default {
         padding-bottom: 10vh;
     }
 
-    &-items {
-    }
-
     &-item {
-        &:not(:first-of-type) {
-            border-top: 1px solid #ddd;
+        // border-bottom: 1px solid c('base-4');
+
+        &::after {
+            content: '';
+            display: block;
+            left: 0;
+            width: 100%;
+            height: 1px;
+            background: c('base-4');
+            background: linear-gradient(to right, c('base-4') 65%, transparent);
         }
-        &--expanded {
-            padding-bottom: 1.5em;
-        }
-    }
-
-    &-button {
-        //RESET BUTTON
-        appearance: none;
-        background: none;
-        border: none;
-        outline: none;
-        color: inherit;
-        font-family: inherit;
-        font-size: inherit;
-        display: grid;
-        align-items: center;
-        grid-template-columns: minmax(0, 1fr) 1.999rem;
-        grid-row-gap: 1.5em;
-        width: 100%;
-        padding: 0;
-        text-align: left;
-        transition: max-height 250ms ease;
-        cursor: pointer;
-
-        &-title {
-        }
-
-        &-icon {
-            font-size: clamp(1.25rem, -0.875rem + 8.333vw, 1.999rem);
-            padding: 1em 0;
-
-            svg {
-                --size: 1.999rem;
-                width: var(--size);
-                height: var(--size);
-                transition: transform 250ms ease;
-
-                path {
-                    fill: c('base-6');
-                }
-            }
-        }
-
-        &--expanded {
-            svg {
-                transform: rotateZ(180deg);
-                opacity: 1;
-            }
-        }
-    }
-
-    &-panel {
-        max-height: 0;
-        padding-right: 4rem;
-        overflow: hidden;
-        transition: max-height 250ms ease;
     }
 }
 </style>
---
+++ slices/Accordion/item.vue
@@ -0,0 +1,154 @@
+<template>
+    <div class="accordion-item" :data-index="index" :class="{ '-expanded': open }">
+        <a :name="encodeURIComponent(item.Title)"></a>
+        <button
+            data-cursor="md"
+            class="accordion-item-button"
+            @click="toggleAccordion()"
+            :aria-expanded="open"
+            :aria-controls="`collapse_${index}`"
+        >
+            <span class="accordion-item-button-icon" v-html="item.Icon" aria-hidden="true"></span>
+            <p class="accordion-item-button-title">{{ item.Title }}</p>
+            <div class="accordion-item-button-indicator" data-anchor data-exclusion>
+                <svg fill="none" viewBox="0 0 25 18" xmlns="http://www.w3.org/2000/svg">
+                    <path
+                        d="m0.25342 2.1106c0 0.89165 0.7334 1.5986 1.5977 1.8176 4.3858 1.1117 8.043 6.5902 8.7347 11.565 0.1254 0.9026 0.8563 1.6443 1.7676 1.6443 0.0084 0 0.0167-1e-4 0.025-2e-4 0.0083 1e-4 0.0167 2e-4 0.025 2e-4 0.9113 0 1.6422-0.7417 1.7682-1.6442 0.6946-4.9728 4.364-10.449 8.7344-11.564 0.8639-0.22043 1.5974-0.92744 1.5974-1.8191s-0.7282-1.6275-1.6075-1.4792c-4.5725 0.77137-8.4439 4.7424-10.518 9.3745-2.0736-4.6321-5.945-8.6031-10.518-9.3745-0.87923-0.14832-1.6075 0.5875-1.6075 1.4792z"
+                        fill="#0B0E11"
+                    />
+                </svg>
+            </div>
+        </button>
+
+        <div
+            class="accordion-item-panel"
+            :aria-expanded="open ? true : false"
+            :aria-hidden="open ? false : true"
+            :id="`collapse_${index}`"
+            ref="accordionPanel"
+            :style="{ '--height': panelHeight + 'px' }"
+        >
+            <prismic-rich-text :field="item.Description" />
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'item',
+    props: {
+        item: Object,
+        index: Number,
+    },
+    data() {
+        return {
+            open: false,
+            panelHeight: 0,
+        }
+    },
+    methods: {
+        toggleAccordion() {
+            this.open = !this.open
+            const panel = this.$refs.accordionPanel
+        },
+    },
+    mounted() {
+        this.panelHeight = this.$refs.accordionPanel.scrollHeight
+
+        window.addEventListener('resize', () => {
+            this.panelHeight = this.$refs.accordionPanel.scrollHeight
+        })
+    },
+}
+</script>
+
+<style lang="scss">
+.accordion-item {
+    svg {
+        width: 100%;
+        height: 100%;
+
+        path {
+            fill: c('base-0');
+        }
+    }
+
+    &-button {
+        //RESET BUTTON
+        appearance: none;
+        background: none;
+        border: none;
+        outline: none;
+        color: inherit;
+        font-family: inherit;
+        font-size: inherit;
+
+        display: grid;
+        align-items: center;
+        grid-template-columns: clamp(1.75rem, 2vw, 2.5rem) auto clamp(1.75rem, 2vw, 2.5rem);
+        grid-column-gap: clamp(0.75rem, 2vw, 1.5rem);
+        width: 100%;
+        padding: 1em 0;
+        text-align: left;
+        transition: max-height 250ms ease;
+        cursor: pointer;
+
+        @include mq('tablet') {
+            padding: 2em 0;
+        }
+
+        &:focus {
+            outline: none;
+        }
+
+        &-icon {
+            margin-bottom: 0.5rem;
+        }
+
+        &-indicator {
+            padding: 0.5em;
+            transition: transform 0.32s cubic-bezier(0.68, -0.55, 0.265, 1.55);
+
+            svg {
+                path {
+                    fill: c('base-3');
+                }
+            }
+        }
+
+        &[aria-expanded='true'] {
+            .accordion-item-button-title,
+            .accordion-item-button-icon svg path {
+                transition: color 0.32s cubic-bezier(0.68, -0.55, 0.265, 1.55) fill 0.32s
+                    cubic-bezier(0.68, -0.55, 0.265, 1.55);
+                color: c('primary-base');
+                fill: c('primary-base');
+            }
+
+            .accordion-item-button-indicator {
+                transform: rotate(180deg);
+            }
+        }
+    }
+
+    &-panel {
+        position: relative;
+        max-height: 0;
+        padding-left: calc(clamp(1.75rem, 2vw, 2.5rem) + clamp(0.75rem, 2vw, 1.5rem));
+        overflow: hidden;
+        transition: max-height 0.32s ease;
+
+        @include mq('tablet') {
+            padding-right: calc(clamp(1.75rem, 2vw, 2.5rem) + clamp(0.75rem, 2vw, 1.5rem));
+        }
+
+        &[aria-expanded='true'] {
+            max-height: var(--height);
+        }
+
+        p:last-of-type {
+            margin-bottom: revert;
+        }
+    }
+}
+</style>
