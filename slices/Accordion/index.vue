80b2a45df389d0aa993aed9a5700a554b074df6b
Kolby Kruger <kolbykruger@gmail.com>
2021-12-17 18:31:50 -0500

    added classnames for title/description

--- slices/Accordion/index.vue
+++ slices/Accordion/index.vue
@@ -1,8 +1,13 @@
 <template>
-    <section class="accordion" data-anim>
+    <section class="accordion" :class="alignment" data-anim>
         <div class="container">
             <div class="accordion-heading" v-if="slice.primary.heading">
-                <prismic-rich-text :field="slice.primary.heading" />
+                <prismic-rich-text class="accordion-heading-title" :field="slice.primary.heading" />
+                <prismic-rich-text
+                    class="accordion-heading-description"
+                    :field="slice.primary.description"
+                    v-if="slice.primary.description"
+                />
             </div>
             <div class="accordion-items">
                 <Item v-for="(item, index) in slice.items" :item="item" :index="index" :key="index" data-anim-child />
@@ -28,6 +33,12 @@ export default {
             default() {
                 return {}
             },
+        },
+    },
+    computed: {
+        alignment() {
+            const alignment = this.slice.primary.Alignment
+            return `accordion-alignment-${alignment == 'Horizontal' ? 'horizontal' : 'vertical'}`
         },
     },
     methods: {
@@ -81,6 +92,34 @@ export default {
             height: 1px;
             background: c('base-4');
             background: linear-gradient(to right, c('base-4') 65%, transparent);
+        }
+    }
+
+    &-alignment {
+        &-horizontal {
+            @include mq('laptop') {
+                margin-top: 18vh;
+
+                .container {
+                    display: grid;
+                    grid-template-columns: repeat(2, 1fr);
+                    grid-gap: 6vw;
+                    max-width: 100%;
+                }
+            }
+
+            .accordion {
+                &-heading {
+                    &-title {
+                        h1,
+                        h2,
+                        h3 {
+                            margin-top: 0;
+                            font-family: $font-1;
+                        }
+                    }
+                }
+            }
         }
     }
 }
