286272a45ed475a73a2cf192375c253ddceb3bd5
Kolby Kruger <kolbykruger@gmail.com>
2021-12-23 15:38:06 -0500

    completely rewrote animations

--- components/Foreward/Foreward.vue
+++ components/Foreward/Foreward.vue
@@ -1,72 +1,19 @@
 <template>
     <section class="foreward">
         <div class="container">
-            <div class="foreward-intro-image">
+            <div class="foreward-image" ref="forewardImage">
                 <!-- <prismic-image :field="document.data.Cover" /> -->
-                <Picture
-                    :field="document.data.Cover"
-                    :sizes="{
-                        'phone-small': '315px',
-                        phone: '400px',
-                        'phone-large': '400px',
-                        tablet: '568px',
-                        'laptop-small': '784px',
-                        laptop: '900px',
-                        'laptop-large': '990px',
-                        'desktop-small': '1160px',
-                        desktop: '1380px',
-                        'desktop-large': '2020px',
-                    }"
-                />
-            </div>
-            <div class="swip">
-                <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    xmlns:xlink="http://www.w3.org/1999/xlink"
-                    width="1488"
-                    height="631"
-                    viewBox="0 0 1488 631"
-                    fill="none"
-                >
-                    <path
-                        class="swip-thick"
-                        d="M-12 523.455C50.5 457.602 155.5 335.104 291.5 353.602C494.479 381.21 587 609.603 530 609.603C482.5 609.603 348.883 300.307 473 204.102C682 42.1038 839 537.603 767.5 568.101C728.084 584.914 645.203 247.59 814 101.102C1014 -72.4653 1415.33 62.7017 1508 107.035"
-                        stroke-width="30"
-                    />
-                    <g opacity="0.15">
-                        <path
-                            class="swip-thick swip-thick-overlay"
-                            d="M-12 523.455C50.5 457.602 155.5 335.104 291.5 353.602C494.479 381.21 587 609.603 530 609.603C482.5 609.603 348.883 300.307 473 204.102C682 42.1038 839 537.603 767.5 568.101C728.084 584.914 645.203 247.59 814 101.102C1014 -72.4653 1415.33 62.7017 1508 107.035"
-                            stroke="url(#pattern)"
-                            stroke-width="30"
-                            style="mix-blend-mode: screen"
-                        />
-                    </g>
-                    <path
-                        class="swip-thin"
-                        d="M-32 543.455C30.5 477.602 135.5 355.104 271.5 373.602C474.479 401.21 567 629.603 510 629.603C462.5 629.603 328.883 320.307 453 224.102C662 62.1038 819 557.603 747.5 588.101C708.084 604.914 625.203 267.59 794 121.102C994 -52.4653 1395.33 82.7017 1488 127.035"
-                    />
-                    <defs>
-                        <pattern
-                            id="pattern"
-                            patternContentUnits="objectBoundingBox"
-                            width="0.069079"
-                            height="0.126135"
-                        >
-                            <use
-                                xlink:href="#image0_701:8"
-                                transform="translate(0.069079) scale(0.000197368 0.000504538) rotate(90)"
-                            />
-                        </pattern>
-                        <image id="image0_701:8" width="250" height="350" xlink:href="/noise/noise.gif" />
-                    </defs>
-                </svg>
+                <Picture :field="document.data.Cover" />
             </div>
         </div>
     </section>
 </template>
 
 <script>
+import { gsap } from 'gsap'
+import { CustomEase } from 'gsap/dist/CustomEase'
+import { ScrollTrigger } from 'gsap/dist/ScrollTrigger'
+
 export default {
     name: 'Foreward',
     props: {
@@ -86,88 +33,74 @@ export default {
             }),
         },
     },
+    computed: {
+        initialLoad() {
+            return this.$store.state.loading.initial
+        },
+    },
+    mounted() {
+        gsap.registerPlugin(CustomEase)
+        gsap.registerPlugin(ScrollTrigger)
+        const ease = CustomEase.create('custom', 'M0,0 C0.215,0.61 0.355,1 1,1 ')
+        const image = this.$refs.forewardImage
+        const i = image.querySelector('img')
+        const timeline = gsap.timeline()
+        const loadType = gsap.set(image, {
+            scale: 1.5,
+            yPercent: 100,
+        })
+
+        this.$router.onReady(() => {
+            this.$nextTick(() => {
+                setTimeout(
+                    () => {
+                        timeline
+                            .to(image, {
+                                yPercent: 8,
+                                duration: 1.2,
+                                ease: 'expo.out',
+                            })
+                            .to(image, 0.75, {
+                                scale: 1,
+                                delay: -0.3,
+                                ease: ease,
+                            })
+                            .to(i, {
+                                scrollTrigger: {
+                                    trigger: image,
+                                    start: 'top bottom',
+                                    scrub: true,
+                                },
+                                yPercent: 18,
+                            })
+                    },
+                    this.initialLoad ? 2000 : 1200
+                ) // page animation transition length
+            })
+        })
+    },
 }
 </script>
 
 <style lang="scss">
 .foreward {
     position: relative;
-    margin-bottom: 20vh;
+    // margin-bottom: 20vh;
+    aspect-ratio: 1.77778 / 1;
 
-    &-intro {
+    &-image {
         position: relative;
-        padding-top: 0;
-        padding-bottom: 0;
-
-        &-image {
-            position: relative;
-            margin: 0 auto;
-            margin-top: 8vh;
-            background: lighten(#0b0e11, 80%);
-            overflow: hidden;
-
-            img {
-                object-fit: cover;
-                width: 100%;
-                height: 100%;
-            }
-        }
-    }
-
-    .swip {
-        position: absolute;
+        margin: 0 auto;
+        overflow: hidden;
         top: 0;
         left: 0;
         width: 100%;
         height: 100%;
-        pointer-events: none;
 
-        svg {
+        img {
+            object-fit: cover;
             width: 100%;
             height: 100%;
-            object-fit: cover;
-            mix-blend-mode: soft-light;
-            transform: scale(1.25) translate(-9%, 6%);
-
-            path {
-                stroke-dasharray: 3050;
-                stroke-dashoffset: 3050;
-                animation: swip-draw 1.82s ease-in-out forwards;
-                animation-delay: 0.86s;
-
-                &.swip-thick {
-                    animation: swip-draw 1.78s ease-in-out forwards;
-                    animation-delay: 0.92s;
-                }
-
-                @keyframes swip-draw {
-                    0% {
-                        stroke-dashoffset: 3050;
-                    }
-                    100% {
-                        stroke-dashoffset: 0;
-                    }
-                }
-            }
-        }
-
-        &-thick {
-            stroke: c('primary-base');
-
-            &-overlay {
-                stroke: url(#a);
-            }
-
-            html[theme='dark'] & {
-                stroke: c('primary-base');
-
-                &-overlay {
-                    stroke: url(#pattern);
-                }
-            }
-        }
-        &-thin {
-            stroke: c('base-0');
         }
     }
 }
