49ba83f8482073353b8ae7f3a16cafb9e211f627
Kolby Kruger <kolbykruger@gmail.com>
2021-12-30 00:50:54 -0500

    new formatting for headings, and image component

--- mixins/serializer/Serializer.js
+++ mixins/serializer/Serializer.js
@@ -2,13 +2,115 @@ export const Serializer = {
     name: 'Serializer',
     methods: {
         htmlSerializer(type, element, content, children) {
+            // Format heading 2 for anchor link and anchor indicator
             if (type == 'heading2') {
                 const id = element.text.replace(/\W+/g, '-').toLowerCase()
-                return `<a class="anchor-link" name="${id}" data-type="h2"></a><h2>${children.join('')}</h2>`
+                return `
+                <h2>
+                    ${children.join('')}
+                    <a class="anchor-link-indicator" aria-hidden="true" tabindex="-1" data-type="h2" href="#${id}" name="${id}">
+                        <span>#</span>
+                    </a>
+                </h2>
+                `
             }
+
+            // Format heading 3 for anchor link and anchor indicator
             if (type == 'heading3') {
                 const id = element.text.replace(/\W+/g, '-').toLowerCase()
-                return `<a class="anchor-link" name="${id}" data-type="h3"></a><h3>${children.join('')}</h3>`
+                return `
+                <h3>
+                    ${children.join('')}
+                    <a class="anchor-link-indicator" aria-hidden="true" tabindex="-1" data-type="h3" href="#${id}" name="${id}">
+                        <span>#</span>
+                    </a>
+                </h3>
+                `
+            }
+            if (type == 'image') {
+                // Break the url on the predefined width
+                const img = element.url.split('&w')[0]
+                // Append the url with the format and clip
+                const imgix = '&fm=webp&fit=clip'
+                // Define the breakpoints for the image
+                const breakpoints = [
+                    {
+                        dimension: 320,
+                        format: 207,
+                    },
+                    {
+                        dimension: 375,
+                        format: 262,
+                    },
+                    {
+                        dimension: 460,
+                        format: 347,
+                    },
+                    {
+                        dimension: 600,
+                        format: 487,
+                    },
+                    {
+                        dimension: 640,
+                        format: 527,
+                    },
+                    {
+                        dimension: 768,
+                        format: 655,
+                    },
+                    {
+                        dimension: 1024,
+                        format: 570,
+                    },
+                    {
+                        dimension: 1200,
+                        format: 739,
+                    },
+                    {
+                        dimension: 1280,
+                        format: 800,
+                    },
+                    {
+                        dimension: 1350,
+                        format: 800,
+                    },
+                    {
+                        dimension: 2560,
+                        format: 800,
+                    },
+                ]
+
+                let sizes = []
+                let sets = []
+
+                // Loop through the breakpoints and format srcset and sizes for picture
+                breakpoints.forEach((size, i) => {
+                    const src = `${img}&w=${size.format}${imgix}`
+                    const isLastItem = i + 1 == breakpoints.length ? true : false
+                    sizes.push(
+                        isLastItem ? `${size.dimension}px` : `(max-width: ${size.dimension}px) ${size.dimension}px`
+                    )
+                    sets.push(`${src} ${size.dimension}w`)
+                })
+
+                // Return the formatted figure with picture
+                return `
+                <figure class="picture" style="aspect-ratio: ${element.dimensions.width} / ${
+                    element.dimensions.height
+                }">
+                    <picture class>
+                        <source type="image/webp"
+                            srcset="${sets.join(', ')}"
+                            sizes="${sizes.join(', ')}"
+                        >
+                        <img src="${element.url}${imgix}"
+                            srcset="${sets.join(',')}"
+                            sizes="${sizes.join(', ')}"
+                            loading="lazy"
+                        >
+                    </picture>
+                </figure>
+                `
             }
         },
     },
