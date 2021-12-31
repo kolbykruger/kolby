99eb7986b501a633f8c505688ed679e760e5c158
Kolby Kruger <kolbykruger@gmail.com>
2021-12-31 02:14:49 -0500

    included date in author -- to be renamed soon

--- components/Author/Author.vue
+++ components/Author/Author.vue
@@ -5,6 +5,7 @@
                 <img src="/images/kolby-squircle.png" alt="Kolby Kruger" />
             </div>
             <p class="author-name">Kolby Kruger</p>
+            <p class="author-date">{{ formatDate }}</p>
         </div>
     </section>
 </template>
@@ -12,21 +13,65 @@
 <script>
 export default {
     name: 'Author',
+    props: {
+        document: Object,
+    },
+    computed: {
+        formatDate() {
+            const d = this.document.last_publication_date
+            const date = new Date(d)
+            const options = {
+                month: 'long',
+                day: 'numeric',
+                year: 'numeric',
+            }
+            return date.toLocaleString('en-us', options)
+        },
+    },
 }
 </script>
 
 <style lang="scss">
 .author {
-    margin-bottom: 0;
+    position: relative;
+    margin-bottom: 3vh;
 
     @include mq('laptop-small') {
-        margin-bottom: 3vh;
+        padding: 2em;
+        margin-left: -2em;
+        margin-right: -2em;
+        margin-bottom: 3em;
+
+        &::after {
+            content: '';
+            position: absolute;
+            top: 0;
+            left: 0;
+            width: 100%;
+            height: 100%;
+            background: c('base-9');
+            border-radius: 0.375em;
+            opacity: 0.35;
+            z-index: -1;
+            pointer-events: none;
+        }
     }
 
     .container {
         display: flex;
         align-items: center;
         flex-flow: row wrap;
+
+        p + p {
+            margin-top: 0;
+            margin-left: 1.5em;
+
+            @include mq('laptop-small') {
+                margin-left: 0;
+                margin-top: 0.25em;
+                opacity: 0.75;
+            }
+        }
 
         @include mq('laptop-small') {
             display: block;
@@ -47,17 +92,22 @@ export default {
     }
 
     &-name {
-        margin-left: 1em;
-        margin-bottom: 0;
         color: c('base-5');
         font-weight: 500;
         @include fs-sm;
+        margin-left: 1em;
+        margin-bottom: 0;
 
         @include mq('laptop-small') {
             @include fs-xxs;
             margin-top: 0.75em;
             margin-left: 0;
         }
+    }
+
+    &-date {
+        margin-top: 0.125em;
+        @include fs-xxs;
     }
 }
 </style>
