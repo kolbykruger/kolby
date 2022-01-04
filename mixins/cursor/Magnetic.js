672500f8321c034fa60874231fca5006dbbeea53
Kolby Kruger <kolbykruger@gmail.com>
2022-01-03 19:52:26 -0500

    magnetic now runs if empty attribute

--- mixins/cursor/Magnetic.js
+++ mixins/cursor/Magnetic.js
@@ -30,7 +30,7 @@ export const Magnetic = {
             magnetic.forEach(magnet => {
                 const attr = magnet.getAttribute('data-magnetic')
 
-                if (!attr) {
+                if (attr === false) {
                     return false
                 }
                 const vals = attr ? attr.split(',') : null
@@ -55,6 +55,7 @@ export const Magnetic = {
                     this.magnetic.active = true
 
                     const bounds = magnet.getBoundingClientRect()
+                    console.log(bounds)
                     this.magnetic.x = bounds.left //- window.pageXOffset
                     this.magnetic.y = bounds.top //- window.pageYOffset
                     this.magnetic.width = bounds.width
