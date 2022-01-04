5b2a681aed693e59627660ed5265601ae9bedf1d
Kolby Kruger <kolbykruger@gmail.com>
2022-01-03 19:52:04 -0500

    created Cases component

---
+++ components/Cases/Cases.vue
@@ -0,0 +1,136 @@
+<template>
+    <section class="cases">
+        <div class="container">
+            <div class="cases-list" data-cursor="invisible">
+                <client-only>
+                    <Flickity ref="flickity" class="cases-slider" :options="flickityOptions" @init="onInit">
+                        <Case v-for="study in cases" :key="study.id" :item="study" />
+                    </Flickity>
+                    <!-- <div class="flickity-indicator" ref="indicator">
+                        <span
+                            class="flickity-indicator-thumb"
+                            :style="{ '--progress': thumbProgress + 'px', '--width': thumbWidth + 'px' }"
+                        ></span>
+                    </div> -->
+                    <Swiper :count="cases.length" :progress="progress" />
+                </client-only>
+            </div>
+            <div class="actions actions-center">
+                <Button location="/work">
+                    Explore more work
+                    <template #posticon><ArrowRight /></template>
+                </Button>
+            </div>
+        </div>
+    </section>
+</template>
+
+<script>
+import Case from '~/components/Cases/Case.vue'
+import Swiper from '~/components/Swiper/Swiper.vue'
+import ArrowRight from '~/assets/svg/arrows/Arrow-Right.svg?inline'
+
+export default {
+    name: 'Cases',
+    props: {
+        cases: Array,
+    },
+    components: {
+        Case,
+        Swiper,
+        ArrowRight,
+    },
+    data() {
+        return {
+            selectedIndex: 0,
+            progress: 0,
+            isMobile: null,
+        }
+    },
+    computed: {
+        flickityOptions() {
+            return {
+                cellSelector: '.case',
+                cellAlign: 'center',
+                contain: true,
+                imagesLoaded: true,
+                adaptiveHeight: false,
+                setGallerySize: true,
+                prevNextButtons: false,
+                freeScroll: true,
+                selectedAttraction: 0.01,
+                friction: 0.18,
+                groupCells: this.groupCells(),
+                initialIndex: this.initialIndex(),
+                pageDots: false,
+            }
+        },
+        size() {
+            const count = this.cases.length
+            return count > 3 ? true : false
+        },
+        indicator() {
+            const count = this.cases.length
+            const index = this.selectedIndex
+        },
+    },
+    methods: {
+        groupCells() {
+            return this.isMobile ? 1 : this.cases.length
+        },
+        initialIndex() {
+            return Math.round(this.cases.length / 2 - 1)
+        },
+        onInit(evt) {
+            this.selectedIndex = evt.selectedIndex
+            evt.on('change', event => {
+                this.selectedIndex = event
+            })
+            evt.on('scroll', progress => {
+                this.progress = progress
+            })
+        },
+    },
+    mounted() {
+        this.isMobile = screen.width <= 768 ? true : false
+
+        window.addEventListener('resize', () => {
+            this.isMobile = screen.width <= 768 ? true : false
+        })
+    },
+}
+</script>
+
+<style lang="scss">
+.cases {
+    .container {
+        padding: 0;
+    }
+
+    &-list {
+        position: relative;
+
+        &::before,
+        &::after {
+            content: '';
+            position: absolute;
+            left: 0;
+            width: 100%;
+            height: 1px;
+            background: linear-gradient(to right, transparent, c('base-4'), transparent);
+        }
+
+        &::before {
+            top: 0;
+        }
+
+        &::after {
+            bottom: 0;
+        }
+    }
+
+    .actions {
+        margin-top: 5vh;
+    }
+}
+</style>
