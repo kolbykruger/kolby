6de90ee6503b02c96b6130c6ac00e02f04421bca
Kolby Kruger <kolbykruger@gmail.com>
2022-01-07 17:04:20 -0500

    configured loading component

--- components/Loading/Loading.vue
+++ components/Loading/Loading.vue
@@ -1,13 +1,13 @@
 <template>
     <div v-if="loading" class="loading-page">
-        <div class="loading"></div>
+        <span class="visually-hidden">LOADING...</span>
     </div>
 </template>
 
 <script>
 export default {
     data: () => ({
-        loading: false
+        loading: false,
     }),
     methods: {
         start() {
@@ -19,37 +19,12 @@ export default {
             this.$nextTick(() => {
                 this.$store.commit('loading/setLoading', { status: false })
             })
-        }
-    }
+        },
+    },
 }
 </script>
 <style>
 .loading-page {
-    position: fixed;
-    top: 0;
-    right: 0;
-    z-index: 1000;
-    padding: 1rem;
-    text-align: center;
-    font-size: 3rem;
-    font-family: sans-serif;
-    display: flex;
-    flex-direction: column;
-    align-items: center;
-    justify-content: center;
-}
-.loading {
-    display: inline-block;
-    width: 1.5rem;
-    height: 1.5rem;
-    border: 4px solid rgba(9, 133, 81, 0.705);
-    border-radius: 50%;
-    border-top-color: #158876;
-    animation: spin 1s ease-in-out infinite;
-}
-@keyframes spin {
-    to {
-        -webkit-transform: rotate(360deg);
-    }
+    z-index: -1;
 }
 </style>
