b63eed6048fb438ce4a2a34c467e6cf58b789685
Kolby Kruger <kolbykruger@gmail.com>
2022-08-12 12:03:53 -0400

    added vh calc on window resize

--- components/Menu/Menu.vue
+++ components/Menu/Menu.vue
@@ -250,6 +250,11 @@ export default {
         // get viewport size on phones
         let vh = window.innerHeight * 0.01
         document.documentElement.style.setProperty('--vh', `${vh}px`)
+
+        window.addEventListener('resize', () => {
+            vh = window.innerHeight * 0.01
+            document.documentElement.style.setProperty('--vh', `${vh}px`)
+        })
     },
     watch: {
         $route() {
