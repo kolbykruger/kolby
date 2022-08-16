a03ef9fc8f4eae23a9e240d1e579e6253f77a0ec
Kolby Kruger <kolbykruger@gmail.com>
2022-08-16 11:07:16 -0400

    adjusted module exports

--- webpack.config.js
+++ webpack.config.js
@@ -1,12 +1,12 @@
 // webpack.config.js
 module.exports = {
-    module: {
-        rules: [
-            {
-                test: /\.(vert|frag)$/i,
-                exclude: /node_modules/,
-                use: ['raw-loader', 'glslify-loader'],
-            },
-        ],
-    },
+    // module: {
+    //     rules: [
+    //         {
+    //             test: /\.(vert|frag)$/i,
+    //             exclude: /node_modules/,
+    //             use: ['raw-loader', 'glslify-loader'],
+    //         },
+    //     ],
+    // },
 }
